d6931cf9b01bd552426b7aa9f026eacef257cdbf
Peng Xiao <xiaoquwl@gmail.com>
2017-06-26 07:05:11 +0800

    s5 overview
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

---
+++ s5_data_structure/overview/syntax.py
@@ -0,0 +1,15 @@
+
+"""
+Indentation is used in Python to delimit blocks. The number of spaces is variable,
+but all statements within the same block must be indented the same amount.
+
+The header line for compound statements, such as if, while, def, and class should be terminated with a colon ( : )
+
+The semicolon ( ; ) is optional at the end of statement.
+"""
+
+if True:
+    print('this is true')
+else:
+    print('False')
+  print('test')
---
+++ s5_data_structure/overview/variable.py
@@ -0,0 +1,19 @@
+
+"""
+Python is dynamically typed. You do not need to declare variables! 
+The declaration happens automatically when you assign a value to a variable.
+Variables can change type, simply by assigning them a new value of a different type.
+Python allows you to assign a single value to several variables simultaneously.
+You can also assign multiple objects to multiple variables.
+"""
+
+a = 100
+b = 'name'
+
+print(a)
+print(b)
+
+c = 200
+c = 'string'
+
+print(c)
