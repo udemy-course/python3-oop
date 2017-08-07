23342c21e54e1f6c113703379793d84012f71d2d
Peng Xiao <xiaoquwl@gmail.com>
2017-08-07 22:48:38 +0800

    exceptions
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- s11_exception/try_except_else_finally.py
+++ s11_exception/try_except_else_finally.py
@@ -2,12 +2,18 @@
 def divide(x, y):
     try:
         result = x / y
+        a = [1]
+        print(a[0])
     except ZeroDivisionError:
         print("division by zero!")
+    except TypeError:
+        print('type error')
+    except Exception:
+        print('base exception')
     else:
         print("result is", result)
     finally:
         print("executing finally clause")
 
 
-divide(1, 0)
+divide(1, 2)
